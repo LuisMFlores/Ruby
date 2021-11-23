79add0af14f478ff8565e6c601caffe6b4de5334
Luis M. Flores <flores_luis_m@outlook.com>
2021-11-23 16:32:03 -0500

    Numbers and Comparisons

--- RubyBasics.rb
+++ RubyBasics.rb
@@ -1,1 +1,23 @@
+# Basics
+
+# Priting text
+
 puts "Hello World\n"
+
+# Numbers
+
+puts 1 + 1 #2
+puts 2 * 2 #4
+puts 9 / 2 #Needs to add decimal to get correct answer
+puts (9.0 + 2) / 2 #Order of operations
+puts  10 % 2 #0
+
+# Comparisons
+
+puts 4 == 3 #false
+puts 4 != 3 #true
+puts 3 < 2 #false
+puts 3 > 2 #true
+puts (5+5) == 10 #true
+puts 3 <= 2 #false
+puts 3 >= 3 #true
