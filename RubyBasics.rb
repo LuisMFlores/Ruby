8d20604ce6058d11a5d7038873fe862bbd0feff3
Luis M. Flores <flores_luis_m@outlook.com>
2021-12-01 12:01:46 -0500

    Arrays

--- RubyBasics.rb
+++ RubyBasics.rb
@@ -342,3 +342,204 @@ end
 puts is_palindrome("racecar")  # => true
 puts is_palindrome("kayak")    # => true
 puts is_palindrome("bootcamp") # => false
+
+#Array
+
+letters = ["a","e","i","o","u"]
+print letters
+
+family_names = [
+    "Luis",
+    "Merlin",
+    "Jean",
+    "Mami",
+    "Papi",
+    "Gordo"
+]
+
+puts family_names #names
+
+numbers = []
+numbers << 1
+numbers << 2
+numbers << 3
+
+puts numbers # 1,2,3
+
+numbers[1] = 5
+puts numbers #1,5,3
+
+def print_array(arr)
+    i = 0
+    while i < arr.length
+        puts arr[i]
+        i += 1
+    end
+end
+
+print_array(family_names) #Prints family_names arr
+print_array(numbers) #Prints numbers arr
+
+def doubler(nums)
+    new_numbers = []
+    i = 0
+    while i < nums.length
+        new_numbers << nums[i] * 2
+        i += 1
+    end
+    new_numbers
+end
+
+print doubler([1, 2, 3, 4]) # => [2, 4, 6, 8]
+puts
+print doubler([7, 1, 8])    # => [14, 2, 16]
+
+def yell(words)
+    yelled_words = []
+    i = 0
+    while i < words.length
+        yelled_words << words[i] + "!"
+        i += 1
+    end
+    yelled_words
+end
+
+puts
+print yell(["hello", "world"]) # => ["hello!", "world!"]
+puts
+print yell(["code", "is", "cool"]) # => ["code!", "is!", "cool!"]
+
+def even_nums(max)
+    even_num_arr = []
+    i = 0
+    while i <= max
+        even_num_arr << i if i % 2 == 0
+        i += 1
+    end
+    even_num_arr
+end
+
+puts
+print even_nums(10) # => [0, 2, 4, 6, 8, 10]
+puts
+print even_nums(5)  # => [0, 2, 4]
+
+def range(min, max)
+    range_arr = []
+    i = min
+    while i <= max
+        range_arr << i
+        i += 1
+    end
+    range_arr
+end
+
+print range(2, 7)   # => [2, 3, 4, 5, 6, 7]
+puts
+print range(13, 20) # => [13, 14, 15, 16, 17, 18, 19, 20]
+
+def odd_range(min, max)
+    odd_range_arr = []
+    i = min
+    while i <= max
+        odd_range_arr << i if i % 2 != 0
+        i += 1
+    end
+    odd_range_arr
+end
+
+print odd_range(11, 18) # => [11, 13, 15, 17]
+puts
+print odd_range(3, 7)   # => [3, 5, 7]
+
+
+def reverse_range(min, max)
+    reversed_range_arr = []
+    i = max - 1
+    while i > min
+        reversed_range_arr << i
+        i -= 1
+    end
+    reversed_range_arr
+end
+
+puts
+print reverse_range(10, 17) # => [16, 15, 14, 13, 12, 11]
+puts
+print reverse_range(1, 7)   # => [6, 5, 4, 3, 2]
+
+def first_half(array)
+    first_half_arr = []
+    middle = array.length / 2.0
+    i = 0
+    while i < middle
+        first_half_arr << array[i]
+        i += 1
+    end
+    first_half_arr
+end
+
+puts
+print first_half(["Brian", "Abby", "David", "Ommi"]) # => ["Brian", "Abby"]
+puts
+print first_half(["a", "b", "c", "d", "e"])          # => ["a", "b", "c"]
+
+def select_odds(numbers)
+    odd_numbers = []
+    i = 0
+    while i < numbers.length
+        odd_numbers << numbers[i] if numbers[i] % 2 != 0
+        i += 1
+    end
+    odd_numbers
+end
+
+puts
+print select_odds([13, 4, 3, 7, 6, 11]) # => [13, 3, 7, 11]
+puts
+print select_odds([2, 4, 6])  
+
+def select_long_words(words)
+    long_word_arr = []
+    i = 0
+    while i < words.length
+        word = words[i]
+        long_word_arr << word if word.length > 4
+        i += 1
+    end
+
+    long_word_arr
+end
+
+print select_long_words(["what", "are", "we", "eating", "for", "dinner"]) # => ["eating", "dinner"]
+puts
+print select_long_words(["keep", "coding"])                               # => ["coding"]
+
+def sum_elements(arr1, arr2)
+    new_ele_arr = []
+    i = 0
+    max_i = arr1.length
+    while i < max_i
+        new_ele_arr << arr1[i] + arr2[i]
+        i += 1
+    end
+    new_ele_arr
+end
+
+print sum_elements([7, 4, 4], [3, 2, 11])                            # => [10, 6, 15]
+puts
+print sum_elements(["cat", "pizza", "boot"], ["dog", "pie", "camp"]) # => ["catdog", "pizzapie", "bootcamp"]
+
+def fizz_buzz(max)
+    new_nums = []
+    i = 1
+    while i < max
+        new_nums << i if (i % 4 == 0 && i % 6 != 0) || (i % 4 != 0 && i % 6 == 0)
+        i += 1
+    end
+    new_nums
+end
+
+print fizz_buzz(20) # => [4, 6, 8, 16, 18]
+puts
+print fizz_buzz(15) # => [4, 6, 8]
