741d64dd41c8d1f3ce4cf600ffea751846450f6f
Luis M. Flores <flores_luis_m@outlook.com>
2021-12-14 15:10:41 -0500

    hash

--- RubyBasics.rb
+++ RubyBasics.rb
@@ -962,3 +962,239 @@ puts pig_latin_word("apple")   # => "appleyay"
 puts pig_latin_word("eat")     # => "eatyay"
 puts pig_latin_word("banana")  # => "ananabay"
 puts pig_latin_word("trash")   # => "ashtray"
+
+# Hashes
+
+my_hash = { "name" => "Luis", "lastName" => "Flores", "age" => 29 }
+puts my_hash["name"] # "Luis"
+
+puts my_hash.has_key?("name") # true
+puts my_hash.has_value?(29) # true
+
+my_hash.each do |key, val|
+    puts key + " belongs to value: " + val.to_s
+end
+
+def char_counter_with_char(set)
+
+    char_hash = Hash.new(0)
+    set.each_char do | char |
+        char_hash[char] += 1
+    end
+    char_hash
+end
+
+puts char_counter_with_char("bootcamp prep")
+
+# Write a method get_double_age that takes in a hash and returns twice the "age" value of the hash.
+
+# acess hash age key and multiply by 2 
+
+def get_double_age(hash)
+    hash["age"] * 2
+end
+
+puts get_double_age({"name"=>"App Academy", "age"=>5}) # => 10
+puts get_double_age({"name"=>"Ruby", "age"=>23})       # => 46
+
+# Write a method get_full_name that takes in a hash containing a first, last, and title. The method should return a string representing the hash's full name
+
+# access the first, last and title keys from hash.
+
+def get_full_name(hash)
+    hash["first"] + " " + hash["last"] + ", " + hash["title"]
+end
+
+hash1 = {"first"=>"Michael", "last"=>"Jordan", "title"=> "GOAT"}
+puts get_full_name(hash1) # => "Michael Jordan, the GOAT"
+
+hash2 = {"first"=>"Fido", "last"=>"McDog", "title"=> "Loyal"}
+puts get_full_name(hash2) # => "Fido McDog, the Loyal"
+
+# Write a method word_lengths that takes in a sentence string and returns a hash where every key is a word of the sentence, and its' corresponding value is the length of that word.
+
+# create empty hash
+# split sentence into arr
+# loop thru arr
+# add word to hash along with length of it
+# return hash
+
+def word_lengths(sentence)
+    sentence_hash = Hash.new()
+    sentence.split(" ").each { |ele| sentence_hash[ele] = ele.length }
+    sentence_hash
+end
+
+puts word_lengths("this is fun") #=> {"this"=>4, "is"=>2, "fun"=>3}
+puts word_lengths("When in doubt, leave it out") #=> {"When"=>4, "in"=>2, "doubt,"=>6, "leave"=>5, "it"=>2, "out"=>3}
+
+# Write a method retrieve_values that takes in two hashes and a key. The method should return an array containing the values from the two hashes that correspond with the given key.
+
+# return hash1 + hash2 with given key parameter as an arr
+
+def retrieve_values(hash1, hash2, key)
+    [hash1[key], hash2[key]]
+end
+
+
+dog1 = {"name"=>"Fido", "color"=>"brown"}
+dog2 = {"name"=>"Spot", "color"=> "white"}
+print retrieve_values(dog1, dog2, "name") #=> ["Fido", "Spot"]
+puts
+print retrieve_values(dog1, dog2, "color") #=> ["brown", "white"]
+puts
+
+# Write a method cat_builder that takes in a name, color, and age. The method should return a hash representing a cat with those values.
+
+def cat_builder(name_str, color_str, age_num)
+    { "name"=>name_str, "color"=>color_str, "age"=>age_num }
+end
+
+print cat_builder("Whiskers", "orange", 3) #=> {"name"=>"Whiskers", "color"=>"orange", "age"=>3}
+puts
+
+print cat_builder("Salem", "black", 100) #=> {"name"=>"Salem", "color"=>"black", "age"=>100}
+puts
+
+# Write a method ae_count that takes in a string and returns a hash containing the number of a's and e's in the string. Assume the string contains only lowercase characters.
+
+# create new hash with default value
+# loop thru each char of parameter
+# add char to new hash if is a or e
+# return new hash
+
+def ae_count(str)
+    str_hash = Hash.new(0)
+    str.each_char { |char| str_hash[char] += 1 if (char == "a" || char == "e") }
+    str_hash
+end
+
+puts ae_count("everyone can program") #=> {"a"=>2, "e"=>3}
+puts ae_count("keyboard") #=> {"a"=>1, "e"=>1}
+
+# Write a method element_count that takes in an array and returns a hash representing the count of each element in the array.
+
+# create new hash
+# loop thru char, increasing value of hash[value] by one
+# return new hash
+
+def element_count(arr)
+    arr_hash = Hash.new(0)
+    arr.each { |char| arr_hash[char] += 1} 
+    arr_hash
+end
+
+puts element_count(["a", "b", "a", "a", "b"]) #=> {"a"=>3, "b"=>2}
+puts element_count(["red", "red", "blue", "green"]) #=> {"red"=>2, "blue"=>1, "green"=>1}
+
+# Write a method select_upcase_keys that takes in a hash and returns a new hash containing key-value pairs of the original hash that had uppercase keys. You can assume that the keys will always be strings.
+
+# create new hash to hold values
+# loop thru hash and check if key is upper case, if it is, add it to new hash
+# return new hash
+
+def select_upcase_keys(hash)
+    selected = {}
+    hash.each_key { |key| selected[key] = hash[key] if key == key.upcase }
+    selected
+end
+
+print select_upcase_keys({"make"=> "Tesla", "MODEL"=> "S", "Year"=> 2018, "SEATS"=> 4}) # => {"MODEL"=>"S", "SEATS"=>4}
+puts
+
+print select_upcase_keys({"DATE"=>"July 4th","holiday"=> "Independence Day", "type"=>"Federal"}) # => {"DATE"=>"July 4th"}
+puts
+
+# Write a method hand_score that takes in a string representing a hand of cards and returns it's total score. You can assume the letters in the string are only A, K, Q, J. A is worth 4 points, K is 3 points, Q is 2 points, and J is 1 point. The letters of the input string not necessarily uppercase.
+
+# create hash containing key/value
+# create sum var
+# iterate thru hand char and add sum with the hash given key
+# return sum var
+def hand_score(hand)
+    card_hash = {"A"=> 4, "K"=> 3, "Q"=>2, "J"=>1}
+    sum = 0
+    hand.upcase.each_char { |key| sum += card_hash[key] }
+    sum
+end
+
+puts hand_score("AQAJ") #=> 11
+puts hand_score("jJka") #=> 9
+
+# Write a method frequent_letters that takes in a string and returns an array containing the characters that appeared more than twice in the string.
+#   
+def frequent_letters(string)
+    hash_ele = element_count(string.split(""))
+    new_element_arr = []
+    hash_ele.each { |key, val| new_element_arr << key if val > 2}
+    new_element_arr
+end
+
+print frequent_letters('mississippi') #=> ["i", "s"]
+puts
+print frequent_letters('bootcamp') #=> []
+puts
+
+# Write a method hash_to_pairs that takes in a hash and returns a 2D array representing each key-value pair of the hash.
+
+# create multi arr
+# iterate thru hash
+# append key/value pairs as array to multi arr
+# return multi arr
+def hash_to_pairs(hash)
+    multi_arr = []
+    hash.each { |k, v| multi_arr << [k, v] }
+    multi_arr
+
+end
+
+
+print hash_to_pairs({"name"=>"skateboard", "wheels"=>4, "weight"=>"7.5 lbs"}) #=> [["name", "skateboard"], ["wheels", 4], ["weight", "7.5 lbs"]]
+puts
+
+
+print hash_to_pairs({"kingdom"=>"animalia", "genus"=>"canis", "breed"=>"German Shepherd"}) #=> [["kingdom", "animalia"], ["genus", "canis"], ["breed", "German Shepherd"]]
+puts
+
+# Write a method unique_elements that takes in an array and returns a new array where all duplicate elements are removed. Solve this using a hash.
+
+# Hint: all keys of a hash are automatically unique
+
+def unique_elements(arr)
+    arr_hash = {}
+    arr.each { |ele| arr_hash[ele] = 1 }
+    arr_hash.keys
+end
+
+print unique_elements(['a', 'b', 'a', 'a', 'b', 'c']) #=> ["a", "b", "c"]
+puts
+
+# Write a method element_replace that takes in an array and a hash. The method should return a new array where elements of the original array are replaced with their corresponding values in the hash.
+
+#define new arr to hold val
+# iterate thru arr
+# check if hash has element as key
+# if it does, get the value and add to the arr if not, add the element to the arr
+#return new arr
+
+def element_replace(arr, hash)
+    new_arr = []
+    arr.each do |ele|
+        if hash.has_key?(ele) 
+            new_arr << hash[ele]
+        else
+            new_arr << ele
+        end
+    end
+    new_arr
+end
+
+arr1 = ["LeBron James", "Lionel Messi", "Serena Williams"]
+hash1 = {"Serena Williams"=>"tennis", "LeBron James"=>"basketball"}
+print element_replace(arr1, hash1) # => ["basketball", "Lionel Messi", "tennis"]
+puts
+
+arr2 = ["dog", "cat", "mouse"]
+hash2 = {"dog"=>"bork", "cat"=>"meow", "duck"=>"quack"}
+print element_replace(arr2, hash2) # => ["bork", "meow", "mouse"]
+puts
