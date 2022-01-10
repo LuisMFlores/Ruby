0d1429ba70cd5d192d031044cabadb0c5bb35696
Luis M. Flores <flores_luis_m@outlook.com>
2022-01-10 14:10:28 -0500

    procs, block and &

--- Foundation/Foundation.rb
+++ Foundation/Foundation.rb
@@ -92,10 +92,64 @@ puts FOOD
 
 # Blocks
 
-[1,2,3].map { |el| -(num * 2)}
+[1,2,3].map { |el| -(el * 2)}
 
 # Using methods with blocks
 
 ["a","b","c"].map { |str| str.upcase }
 ["a","b","c"].map(&:upcase)
 [1,2,3].select(&:odd?)
+
+# Procs
+
+doubler = Proc.new { |num| num * 2}
+p doubler.call(2)
+
+is_even = Proc.new do |num|
+    if num % 2 == 0
+        true
+    else
+        false
+    end
+end
+
+puts is_even.call(2) # True
+
+# Passing proc to method
+
+def add_and_proc(num_1, num_2, prc)
+    sum = num_1 + num_2
+    puts prc.call(sum)
+end
+
+negate = Proc.new { |num| -1 * num }
+square = Proc.new { |num| num * num }
+
+
+add_and_proc(1,2,square) #9
+add_and_proc(5,5, negate) #-10
+
+# Passing block to a method and convert to a proc
+
+def new_add_and_proc(num_1, num_2, &prc)
+    sum = num_1 + num_2
+    puts prc.call(sum)
+end
+
+new_add_and_proc(2,2) { |num| num * num } # 16 Doubler proc
+new_add_and_proc(5,3) { |num| num % 2 == 0 } #true is even proc
+
+# Using & to transform from prock to block
+
+def new_add_sum_proc(num_1, num_2, &proc)
+    puts proc.call(num_1 + num_2)
+end
+
+new_doubler = Proc.new { |num| num * 2}
+
+new_add_sum_proc(1,2,&new_doubler) # 6
+
+# another & example
+
+newest_dobler = Proc.new { |num| num * 2 }
+p [1,2,3,4,5].map(&newest_dobler) # [2,4,6,8,10]
