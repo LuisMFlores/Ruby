ab926d7a6d47441afed11324eeeef040ada0a120
Luis M. Flores <flores_luis_m@outlook.com>
2022-03-11 15:09:39 -0500

    Game over method implemented

--- MemoryPuzzleGame.rb
+++ MemoryPuzzleGame.rb
@@ -1,5 +1,34 @@
 require_relative "Board"
+require_relative "Player"
 
 class MemoryPuzzleGame
+
+    def initialize(name)
+        @board = Board.new
+        @player = Player.new(name)
+        run
+    end
+
+    def run
+        welcome_message
+
+        until game_over?
+
+        end
+
+    end
+
+    def game_over?
+        @board.cards.none { |card| card.hidden == false }
+    end
+
+    private 
+
+    def welcome_message
+        puts "-------------------------------"
+        puts "|Welcome to Memory-Puzzle-Game|"
+        puts "-------------------------------"
+        puts
+    end
 
 end
