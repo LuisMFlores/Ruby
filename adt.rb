203ae212520df66f2e454515d777b1bcc7377b23
Luis M. Flores <flores_luis_m@outlook.com>
2022-03-30 16:48:23 -0400

    map class

--- adt.rb
+++ adt.rb
@@ -47,3 +47,38 @@ class Queue
     
     attr_reader :underlying_array
 end
+
+class Map
+
+    def initialize
+        @underlying_array = [[]]
+    end
+
+    def set(k,v)
+        pair_index = underlying_array.index { |pair| pair.first == k }
+        pair_index ? underlying_array[pair_index].last = v : underlying_array.push([k,v])
+    end
+
+    def get(k)
+        underlying_array.each { |pair| return pair if pair.last == k }
+        nil
+    end
+
+    def delete(k)
+        v = get(k)
+        underlying_array.reject! { |pair| pair.first == k }
+        v
+    end
+
+    def show
+        deep_dup(underlying_array)
+    end
+
+    private
+
+    attr_reader :underlying_array
+
+    def deep_dup
+        underlying_array.map { |el| el.is_a?(Array) ? deep_dup(el) : el }
+    end
+end
