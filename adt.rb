850fcc2b1d18c35f6a79f5c1e9a3d790e249ff0f
Luis M. Flores <flores_luis_m@outlook.com>
2022-03-30 15:55:16 -0400

    queue class

--- adt.rb
+++ adt.rb
@@ -21,5 +21,29 @@ class Stack
     private
 
   attr_reader :underlying_array
-  
+
+end
+
+class Queue
+
+    def initialize
+        @underlying_array = []
+    end
+
+    def enqueue(el)
+        underlying_array.push(el)
+        el
+    end
+
+    def dequeue
+        underlying_array.shift
+    end
+
+    def peek
+        underlying_array.first
+    end
+
+    private
+    
+    attr_reader :underlying_array
 end
