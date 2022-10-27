d241b0109e2d7ef1b870cb4c9822878d7aaf37b8
Luis M. Flores <flores_luis_m@outlook.com>
2022-10-27 16:35:13 -0400

    basic numbers and comparable operators

---
+++ Intro.rb
@@ -0,0 +1,25 @@
+# Numbers
+
+puts 10 / 3 # 3
+puts 27 % 6 # 3
+puts 5 + 5 / 2.0 # 7.5
+
+# Numbers Exercise 1
+# Write 4 examples of expressions using each of the operations: +, -, /, and %. 
+# Print out the result of your expressions using puts. Be sure to test your work by running your code!
+
+puts 10 + 10 # 20
+puts 1 - 1 # 0
+puts 9 / 3 # 3
+puts 25 % 5 # 0
+
+# Numbers Exercise 2
+# Write 6 comparison expressions using each of the operations: ==, !=, <, >, <=, >=. 
+# Print out the result of your expressions using puts. Be sure to test your work by running your code!
+
+puts 10 == 10 # true
+puts 10 != 1 # true
+puts 5 > 2 # true
+puts 5 < 1 # false
+puts 5 >= 6 # false
+puts 6 <= 5 # false
