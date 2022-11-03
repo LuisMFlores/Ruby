1047b6e0ac514914729f1cf67e400d06146b3d1c
Luis M. Flores <flores_luis_m@outlook.com>
2022-11-03 15:05:07 -0400

    hash exercise

--- Intro.rb
+++ Intro.rb
@@ -1012,3 +1012,194 @@ dog["enemies"] << "cats"
 puts dog
 
 puts dog.has_key?("location") # true
+
+dog.each do |k, v|
+    puts "-" + k
+    puts "--" + v.to_s 
+end
+
+dog.each_key do |k|
+    puts k
+end
+
+dog.each_value do |v|
+    puts v
+end
+
+counter = Hash.new(0)
+
+str = "bootcamp"
+str.each_char do |char|
+    counter[char] += 1
+end
+
+puts counter
+
+key = "o"
+
+puts counter[key] # 2
+
+mississipi_river_str = "Mississipi River"
+hash_counter = Hash.new(0)
+
+mississipi_river_str.each_char { |char| hash_counter[char] += 1 }
+
+sorted = hash_counter.sort_by { |k,v| v }
+
+puts sorted[-1][0] # i
+
+def get_double_age(hash)
+    hash["age"] * 2
+end
+
+puts get_double_age({"name"=>"App Academy", "age"=>5}) # => 10
+puts get_double_age({"name"=>"Ruby", "age"=>23})       # => 46
+
+def get_full_name(hash)
+    hash["first"] + " " + hash["last"] + ", the " + hash["title"]
+end
+
+hash1 = {"first"=>"Michael", "last"=>"Jordan", "title"=> "GOAT"}
+puts get_full_name(hash1) # => "Michael Jordan, the GOAT"
+
+hash2 = {"first"=>"Fido", "last"=>"McDog", "title"=> "Loyal"}
+puts get_full_name(hash2) # => "Fido McDog, the Loyal"
+
+def word_lengths(sentence)
+    word_length_hash = {}
+    sentence.split(" ").each { |word| word_length_hash[word] = word.length }
+    word_length_hash
+end
+
+puts word_lengths("this is fun") #=> {"this"=>4, "is"=>2, "fun"=>3}
+puts word_lengths("When in doubt, leave it out") #=> {"When"=>4, "in"=>2, "doubt,"=>6, "leave"=>5, "it"=>2, "out"=>3}
+
+def retrieve_values(hash1, hash2, key)
+    [hash1[key], hash2[key]]
+end
+
+
+dog1 = {"name"=>"Fido", "color"=>"brown"}
+dog2 = {"name"=>"Spot", "color"=> "white"}
+print retrieve_values(dog1, dog2, "name") #=> ["Fido", "Spot"]
+puts
+print retrieve_values(dog1, dog2, "color") #=> ["brown", "white"]
+puts
+
+
+def cat_builder(name_str, color_str, age_num)
+    {"name" => name_str, "color" => color_str, "age" => age_num }
+end
+
+print cat_builder("Whiskers", "orange", 3) #=> {"name"=>"Whiskers", "color"=>"orange", "age"=>3}
+puts
+
+print cat_builder("Salem", "black", 100) #=> {"name"=>"Salem", "color"=>"black", "age"=>100}
+puts
+
+def ae_count(str)
+    str_ae_hash = Hash.new(0)
+    str.each_char do |char|
+        str_ae_hash[char] += 1 if char == "a" || char == "e"
+    end
+
+    str_ae_hash
+end
+
+puts ae_count("everyone can program") #=> {"a"=>2, "e"=>3}
+puts ae_count("keyboard") #=> {"a"=>1, "e"=>1}
+
+def element_count(arr)
+    count_hash = Hash.new(0)
+    arr.each { |ele| count_hash[ele] += 1 }
+    count_hash
+end
+
+puts element_count(["a", "b", "a", "a", "b"]) #=> {"a"=>3, "b"=>2}
+puts element_count(["red", "red", "blue", "green"]) #=> {"red"=>2, "blue"=>1, "green"=>1}
+
+def select_upcase_keys(hash)
+    upcase_hash = Hash.new("")
+    hash.each_key { |key| upcase_hash[key] = hash[key] if key.upcase == key }
+    upcase_hash
+
+end
+
+print select_upcase_keys({"make"=> "Tesla", "MODEL"=> "S", "Year"=> 2018, "SEATS"=> 4}) # => {"MODEL"=>"S", "SEATS"=>4}
+puts
+
+print select_upcase_keys({"DATE"=>"July 4th","holiday"=> "Independence Day", "type"=>"Federal"}) # => {"DATE"=>"July 4th"}
+puts
+
+def hand_score(hand)
+    hash_score = {
+        "a" => 4, 
+        "k" => 3, 
+        "q" => 2, 
+        "j" => 1 
+    }
+
+    sum = 0
+    hand.each_char { |char| sum += hash_score[char.downcase] }
+    sum
+end
+
+puts hand_score("AQAJ") #=> 11
+puts hand_score("jJka") #=> 9
+
+def frequent_letters(string)
+    counter_hash = Hash.new(0)
+    string.each_char { |char| counter_hash[char] += 1}
+    char_arr = []
+
+    counter_hash.each_key { |key| char_arr << key if counter_hash[key] > 2 }
+    char_arr
+end
+
+print frequent_letters('mississippi') #=> ["i", "s"]
+puts
+print frequent_letters('bootcamp') #=> []
+puts
+
+def hash_to_pairs(hash)
+    hash.sort_by { |k, v| k }
+end
+
+
+print hash_to_pairs({"name"=>"skateboard", "wheels"=>4, "weight"=>"7.5 lbs"}) #=> [["name", "skateboard"], ["wheels", 4], ["weight", "7.5 lbs"]]
+puts
+
+
+print hash_to_pairs({"kingdom"=>"animalia", "genus"=>"canis", "breed"=>"German Shepherd"}) #=> [["kingdom", "animalia"], ["genus", "canis"], ["breed", "German Shepherd"]]
+puts
+
+# Hint: all keys of a hash are automatically unique
+
+def unique_elements(arr)
+    hash = Hash.new(0)
+    arr.each { |ele| hash[ele] += 1 }
+    hash.keys
+end
+
+print unique_elements(['a', 'b', 'a', 'a', 'b', 'c']) #=> ["a", "b", "c"]
+puts
+
+def element_replace(arr, hash)
+    arr.map do |ele|
+        if hash.has_key?(ele)
+            hash[ele]
+        else
+            ele
+        end
+    end
+end
+
+arr1 = ["LeBron James", "Lionel Messi", "Serena Williams"]
+hash1 = {"Serena Williams"=>"tennis", "LeBron James"=>"basketball"}
+print element_replace(arr1, hash1) # => ["basketball", "Lionel Messi", "tennis"]
+puts
+
+arr2 = ["dog", "cat", "mouse"]
+hash2 = {"dog"=>"bork", "cat"=>"meow", "duck"=>"quack"}
+print element_replace(arr2, hash2) # => ["bork", "meow", "mouse"]
+puts
