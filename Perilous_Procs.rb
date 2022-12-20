7395ace410bc4bc8e67ade0025ab74d0edeba10f
Luis M. Flores <flores_luis_m@outlook.com>
2022-12-20 13:54:57 -0500

    perilous_procs part 2

--- Perilous_Procs.rb
+++ Perilous_Procs.rb
@@ -103,3 +103,80 @@ p first_index(['bitten', 'bit', 'cat', 'byte', 'below']) { |el| el.length > 6 } 
 p first_index(['bit', 'cat', 'byte', 'below']) { |el| el[0] == 'b' }            # 0
 p first_index(['bit', 'cat', 'byte', 'below']) { |el| el.include?('a') }        # 1
 p first_index(['bit', 'cat', 'byte', 'below']) { |el| el[0] == 't' }  #nil
+
+def xnor_select(arr, prc_1, prc_2)
+    arr.select {|el| prc_1.call(el) == prc_2.call(el) }
+end
+
+is_even = Proc.new { |n| n % 2 == 0 }
+is_odd = Proc.new { |n| n % 2 != 0 }
+is_positive = Proc.new { |n| n > 0 }
+
+p xnor_select([8, 3, -4, -5], is_even, is_positive)         # [8, -5]
+p xnor_select([-7, -13, 12, 5, -10], is_even, is_positive)  # [-7, -13, 12]
+p xnor_select([-7, -13, 12, 5, -10], is_odd, is_positive)   # [5, -10]
+
+def filter_out!(arr, &prc)
+    # filter = false
+    
+    # while !filter
+    #     filter = true
+    #     arr.each_with_index do |el, idx|
+    #         if prc.call(el)
+    #             arr.delete_at(idx)
+    #             filter = false
+    #             break
+    #         end
+    #     end
+    # end
+
+    # arr
+
+    arr.uniq.each { |el| arr.delete(el) if prc.call(el) }
+end
+
+arr_1 = [10, 6, 3, 2, 5 ]
+filter_out!(arr_1) { |x| x.odd? }
+p arr_1     # [10, 6, 2]
+
+arr_2 = [1, 7, 3, 5 ]
+filter_out!(arr_2) { |x| x.odd? }
+p arr_2     # []
+
+arr_3 = [10, 6, 3, 2, 5 ]
+filter_out!(arr_3) { |x| x.even? }
+p arr_3     # [3, 5]
+
+arr_4 = [1, 7, 3, 5 ]
+filter_out!([1, 7, 3, 5 ]) { |x| x.even? }
+p arr_4 # [1, 7, 3, 5]
+
+def multi_map(arr, n = 1, &prc)
+    arr.map do |el|
+        n.times { |i| el = prc.call(el) }
+        el
+    end
+end
+
+p multi_map(['pretty', 'cool', 'huh?']) { |s| s + '!'}      # ["pretty!", "cool!", "huh?!"]
+p multi_map(['pretty', 'cool', 'huh?'], 1) { |s| s + '!'}   # ["pretty!", "cool!", "huh?!"]
+p multi_map(['pretty', 'cool', 'huh?'], 3) { |s| s + '!'}   # ["pretty!!!", "cool!!!", "huh?!!!"]
+p multi_map([4, 3, 2, 7], 1) { |num| num * 10 }             # [40, 30, 20, 70]
+p multi_map([4, 3, 2, 7], 2) { |num| num * 10 }             # [400, 300, 200, 700]
+p multi_map([4, 3, 2, 7], 4) { |num| num * 10 }             # [40000, 30000, 20000, 70000]
+
+def proctition(arr, &prc)
+    true_eles = []
+    false_eles = []
+    arr.each { |el| prc.call(el) ? true_eles << el : false_eles << el }
+    true_eles + false_eles
+end
+
+p proctition([4, -5, 7, -10, -2, 1, 3]) { |el| el > 0 }
+# [4, 7, 1, 3, -5, -10, -2]
+
+p proctition([7, 8, 3, 6, 10]) { |el| el.even? }
+# [8, 6, 10, 7, 3]
+
+p proctition(['cat','boot', 'dog', 'bug', 'boat']) { |s| s[0] == 'b' }
+# ["boot", "bug", "boat", "cat", "dog"]
